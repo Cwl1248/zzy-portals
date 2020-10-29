c1880e8ea1c8fe84da2de10036dee37ddb1cf597
chf <577690236@qq.com>
2020-10-29 15:22:49 +0800

    初始上传中医药网前台

---
+++ src/main/java/com/zzyportals/ZzyPortalsApplication.java
@@ -0,0 +1,13 @@
+package com.zzyportals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzyPortalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzyPortalsApplication.class, args);
+    }
+
+}
