4b59aab101d0e36f1f4eb6c79ddbf4b14cfb4127
beason353739 <2368702029@qq.cpm>
2020-10-30 09:34:09 +0800

    分支测试

---
+++ src/main/java/com/zzyportals/controller/news/NewsController.java
@@ -0,0 +1,14 @@
+package com.zzyportals.controller.news;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author ：ChengWl
+ * @date ：Created in 2020/10/30
+ * @description：新闻模块控制器
+ */
+@Controller
+public class NewsController {
+
+
+}
---
+++ src/main/java/com/zzyportals/controller/notice/NoticeController.java
@@ -0,0 +1,9 @@
+package com.zzyportals.controller.notice;
+
+/**
+ * @author ：ChengWl
+ * @date ：Created in 2020/10/30
+ * @description：TODO
+ */
+public class NoticeController {
+}
